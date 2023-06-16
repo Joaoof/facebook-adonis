aa844587b93059112693942dab8479757ff86609
Joaoof <joaodeus400@gmail.com>
2023-06-16 07:24:19 -0300

    enviando-email

--- app/Controllers/Http/Users/Register.ts
+++ app/Controllers/Http/Users/Register.ts
@@ -2,6 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { StoreValidator } from 'App/Validators/User/Register'
 import { User, UserKey } from 'App/Models'
 import { faker } from '@faker-js/faker'
+import Mail from '@ioc:Adonis/Addons/Mail'
 
 export default class UserRegisterController {
   public async store({ request }: HttpContextContract) {
@@ -19,6 +20,13 @@ export default class UserRegisterController {
     user.related('keys').create(keyData)
 
     const link = `${redirectUrl.replace(/\/$/, '')}/${key}`
+
+    Mail.send((message) => {
+      message.to(email)
+      message.from('contato@facebook.com', 'Facebook')
+      message.subject('Criação de conta')
+      message.htmlView('emails/register', { link })
+    })
   }
 
   public async show({}: HttpContextContract) {}
--- app/Models/UserKey.ts
+++ app/Models/UserKey.ts
@@ -1,14 +1,16 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
 import { User } from 'App/Models'
 
 export default class UserKey extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  @column()
+  public key: string
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  @column()
+  public userId: number
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
 }
