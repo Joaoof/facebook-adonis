9cf95d77b054a07faea7607211c3f932014b5e90
Joaoof <joaodeus400@gmail.com>
2023-06-26 08:34:49 -0300

    resolvendo desafio da senha esquecida hehe

--- start/routes/users.ts
+++ start/routes/users.ts
@@ -3,6 +3,7 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.post('/users/register', 'Users/Register.store')
 Route.get('/users/register/:key', 'Users/Register.show')
 Route.put('/users/register', 'Users/Register.update')
-Route.post('/users/register', 'Users/ForgotPasswords.store')
+
+Route.post('/users/forgot-password', 'Users/ForgotPasswords.store')
 Route.get('/users/register/:key', 'Users/ForgotPasswords.show')
 Route.put('/users/register', 'Users/ForgotPasswords.update')
